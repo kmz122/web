a3551247597c93f05acd85c5fcb9f40987aa9809
Kyi Myo Zaw <kmzgithub122@gmail.com>
2022-03-14 00:08:10 +0630

    Adding alert! & Formmating & cleaning the code.

--- circle-progress-bar/scripts.js
+++ circle-progress-bar/scripts.js
@@ -1,26 +1,31 @@
-const halfCircles = document.querySelectorAll('.half-circle');
-console.log(halfCircles)
-const halfCircleTop = document.querySelector('.half-circle-top');
-const progressBarCircle = document.querySelector('.progress-bar-circle');
+const halfCircles = document.querySelectorAll(".half-circle");
+console.log(halfCircles);
+const halfCircleTop = document.querySelector(".half-circle-top");
+const progressBarCircle = document.querySelector(".progress-bar-circle");
 
-document.addEventListener('scroll', () => {
-    const pageViewPortHeight = window.innerHeight;
-    const pageHeight = document.documentElement.scrollHeight;
-    const scrolledPortion = window.pageYOffset;
-    const scrolledPortionDegree = (scrolledPortion / (pageHeight - pageViewPortHeight) * 360);
+document.addEventListener("DOMContentLoaded", () =>
+  alert('Scroll Down/Up to See the "Circle Progress Bar" working!!')
+);
+document.addEventListener("scroll", () => {
+  const pageViewPortHeight = window.innerHeight;
+  const pageHeight = document.documentElement.scrollHeight;
+  const scrolledPortion = window.pageYOffset;
+  const scrolledPortionDegree =
+    (scrolledPortion / (pageHeight - pageViewPortHeight)) * 360;
 
-    const scrolledPortionPercent = Math.floor(scrolledPortionDegree / 360 * 100);
-    progressBarCircle.textContent = `${scrolledPortionPercent}%`
+  const scrolledPortionPercent = Math.floor(
+    (scrolledPortionDegree / 360) * 100
+  );
+  progressBarCircle.textContent = `${scrolledPortionPercent}%`;
 
-    halfCircles.forEach(el => {
-        el.style.transform = `rotate(${scrolledPortionDegree}deg)`;
+  halfCircles.forEach((el) => {
+    el.style.transform = `rotate(${scrolledPortionDegree}deg)`;
 
-        if (scrolledPortionDegree >= 180) {
-            halfCircles[0].style.transform = 'rotate(180deg)';
-            halfCircleTop.style.opacity = '0';
-        } else {
-            halfCircleTop.style.opacity = '1';
-        }
-    });
-    
-})
+    if (scrolledPortionDegree >= 180) {
+      halfCircles[0].style.transform = "rotate(180deg)";
+      halfCircleTop.style.opacity = "0";
+    } else {
+      halfCircleTop.style.opacity = "1";
+    }
+  });
+});
