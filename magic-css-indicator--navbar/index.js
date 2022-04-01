856047ab88365fde7108cb77970af12e5a722af2
Kyi Myo Zaw <kmzgithub122@gmail.com>
2022-04-01 18:39:15 +0630

    Magic CSS Indicator Navbar - Making smooth transition & changing indicator to ul-scope.

--- magic-css-indicator--navbar/index.js
+++ magic-css-indicator--navbar/index.js
@@ -1,11 +1,16 @@
+document.addEventListener("click", (event) => {
+  const targetEle = event.target;
+  const allLiArr = [...document.querySelectorAll(".nav-items > li")];
+  const activeLi = targetEle.closest("li");
+  const indicator = document.querySelector(".indicator");
 
+  //   Working only if one of the 'li' is target element, if not default
+  if (activeLi) {
+    document
+      .querySelectorAll("li")
+      .forEach((ele) => ele.classList.remove("active"));
 
-document.addEventListener('click', event => {
-    const targetEle = event.target;
-
-    const activeLi = targetEle.closest('li');
-
-    document.querySelectorAll('li').forEach(ele => ele.classList.remove('active'));
-
-    activeLi.classList.add('active');
-})
+    activeLi.classList.add("active");
+    indicator.style.setProperty("--position", allLiArr.indexOf(activeLi));
+  }
+});
