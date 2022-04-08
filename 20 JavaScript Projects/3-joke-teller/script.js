61dedfefc28fc45e3c43d6f35827f8ed87c36d90
Kyi Myo Zaw <kmzgithub122@gmail.com>
2022-04-08 23:42:38 +0630

    JS 20 Projects > Joke Teller > 2nd commit - add joke api

--- 20 JavaScript Projects/3-joke-teller/script.js
+++ 20 JavaScript Projects/3-joke-teller/script.js
@@ -105,17 +105,36 @@ const VoiceRSS = {
   },
 };
 
-function test() {
-  VoiceRSS.speech({
-    key: "d7179facf6f24e09ad1cda50d5c9f1f1",
-    src: "Hello, world!",
-    hl: "en-us",
-    v: "Linda",
-    r: 0,
-    c: "mp3",
-    f: "44khz_16bit_stereo",
-    ssml: false,
-  });
+// function test() {
+//   VoiceRSS.speech({
+//     key: "d7179facf6f24e09ad1cda50d5c9f1f1",
+//     src: "Hello, world!",
+//     hl: "en-us",
+//     v: "Linda",
+//     r: 0,
+//     c: "mp3",
+//     f: "44khz_16bit_stereo",
+//     ssml: false,
+//   });
+// }
+
+// test();
+
+// Get Joke from Joke API
+async function getJokes() {
+  let joke = "";
+  const jokeApiUrl =
+    "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit";
+  try {
+    const response = await fetch(jokeApiUrl);
+    const data = await response.json();
+
+    if (!data.joke) joke = `${data.setup} ... ${data.delivery}`;
+    else joke = data.joke;
+  } catch (error) {
+    // Catch Errors Here
+    console.log("whoops", error);
+  }
 }
 
-test();
+getJokes();
